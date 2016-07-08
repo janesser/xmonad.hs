92640bf3acc5a135d55febd358eae992bed0958e
Jan Esser (MGM) <jan.esser@mgm-tp.com>
2016-07-08 11:55:13 +0200

    fixed double usage of M-l.

--- xmonad.hs
+++ xmonad.hs
@@ -24,7 +24,7 @@ myConfig = defaultConfig {
     -- bzr branch bzr://anamnesis.bzr.sourceforge.net/bzrroot/anamnesis trunk
     ("M-c", spawn "anamnesis -b"),
     ("M-<Print>", spawn "shutter -s"),
-    ("M-l", spawn "xscreensaver-command --lock")]
+    ("M-L", spawn "xscreensaver-command --lock")]
 
 myLogHook spw = dynamicLogWithPP xmobarPP {
   ppOutput = hPutStrLn $ spw
