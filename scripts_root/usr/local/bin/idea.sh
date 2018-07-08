c1be3362cb474a9ea669963fc5f4f6af3582f76e
jan@pidgin <jan@esserjan.de>
2018-07-08 20:04:30 +0200

    Support intellij bundled JRE.

--- scripts_root/usr/local/bin/idea.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-_JAVA_AWT_WM_NONREPARENTING=1 intellij.idea -> ../idea-IC-145.972.3/bin/idea.sh
--- scripts_root/usr/local/bin/intellij.sh
+++ scripts_root/usr/local/bin/intellij.sh
@@ -1,1 +1,15 @@
-_JAVA_AWT_WM_NONREPARENTING=1 SWT_GTK3=0 /usr/local/bin/idea.sh
+STARTER=`which intellij-idea-community`
+
+## JAVA_HOME
+# override with bundled when present
+BUNDLED_JRE=/opt/intellij-idea-community/jre64/
+if [ -f $BUNDLED_JRE ]; then
+  export JAVA_HOME=$BUNDLED_JRE
+  export PATH=/opt/intellij-idea-community/jre64/bin:$PATH
+fi
+
+## DETECT & RUN STARTER
+# gtk2-mode & xmonad tweak
+if [ -f $STARTER ]; then
+  _JAVA_AWT_WM_NONREPARENTING=1 SWT_GTK3=0 $STARTER $*
+fi
