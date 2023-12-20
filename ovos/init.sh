aea9e4fd4dcb826f457c30359f878c6213b1c903
Jan Esser <JEsser@gmx.de>
2023-12-20 15:13:13 +0100

    ovos de-de setup.

---
+++ ovos/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pushd .
+mkdir -p ~/projs
+cd ~/projs
+git clone https://github.com/OpenVoiceOS/ovos-docker.git
+cd ovos-docker/compose
+cp .env.example .env
+podman-compose -f docker-compose.yml -f docker-compose.skills.yml up -d
+
+# for german use mycroft.conf and audio.list
+
+# podman-compose -f docker-compose.yml -f docker-compose.skills.yml logs ovos_core
+# podman-compose -f docker-compose.yml -f docker-compose.skills.yml logs ovos_audio
+
+# podman-compose -f docker-compose.yml -f docker-compose.skills.yml exec ovos_core bash
+## try ovos-config, ovos-simple-cli
+
+# podman-compose -f docker-compose.yml -f docker-compose.skills.yml exec ovos_audio bash
+## try ovos-speak
