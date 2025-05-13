5335aa19883684201a50795dfeeae5537d69b5e4
Jan Esser <jesser@gmx.de>
2025-05-13 20:14:12 +0200

    fix nala setup

--- run_once_2_nala_install.sh
+++ run_once_2_nala_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 sudo apt install nala
-sudo nala fetch --auto
-nala --install-completion=fish --show-completion=fish
+#sudo nala fetch
+sudo nala update
+
+NALA_VERSION=`nala --version`
+
+if [[ $NALA_VERSION == "nala 0.15.1" ]]; then
+    nala --install-completion=fish
+else
+    nala --install-completion
+fi
