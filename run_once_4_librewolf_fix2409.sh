bfa5d7f6941385931ba98d9e5ff3fcf726b7cc83
Jan Esser <jesser@gmx.de>
2025-09-25 13:25:44 +0200

    fix librewolf missing apparmor

---
+++ run_once_4_librewolf_fix2409.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://codeberg.org/librewolf/issues/issues/2409
+
+echo """# This profile allows everything and only exists to give the
+# application a name instead of having the label "unconfined"
+abi <abi/4.0>,
+include <tunables/global>
+profile librewolf /usr/share/librewolf/{librewolf,librewolf-bin} flags=(unconfined) {
+        userns,
+        # Site-specific additions and overrides. See local/README for details.
+        include if exists <local/librewolf>
+}
+""" | sudo tee /etc/apparmor.d/librewolf
+
+sudo systemctl reload apparmor
