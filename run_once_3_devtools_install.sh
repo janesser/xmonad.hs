fc85c0d0b6cec17f61c85a8bb27c6bba4651a9a9
Jan Esser <jesser@gmx.de>
2025-10-02 14:31:15 +0200

    git config global

--- run_once_3_devtools_install.sh
+++ run_once_3_devtools_install.sh
@@ -5,3 +5,6 @@ sudo apt install git gitk git-lfs
 sudo apt install snap
 sudo snap install kubectl --classic
 sudo snap install helm --classic
+
+git config --global user.name "Jan Esser"
+git config --global user.email "jesser@gmx.de"
