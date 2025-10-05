4fce13f2c96f5c6cdafda727b1f0f042b20ea95e
Jan Esser <jesser@gmx.de>
2025-10-05 21:57:30 +0200

    install fisher plugin manager and sdkman

--- run_once_3_devtools_install.sh
+++ run_once_3_devtools_install.sh
@@ -7,5 +7,5 @@ sudo snap install kubectl --classic
 sudo snap install helm --classic
 
 # TODO https://sdkman.io/install/
-# sdkman is stronly bash based and requires a wrapper like this: https://github.com/reitzig/sdkman-for-fish
-# curl -s "https://get.sdkman.io" | bash
+## run_once_1_fish_2fisher.sh: see reitzig/sdkman-for-fish
+curl -s "https://get.sdkman.io" | bash
