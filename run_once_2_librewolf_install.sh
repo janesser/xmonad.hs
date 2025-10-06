f4b570a3ce58d7f7f46c5243c0c4e42954f37776
Jan Esser <jesser@gmx.de>
2025-10-06 21:51:23 +0200

    use extrepo robustly

--- run_once_2_librewolf_install.sh
+++ run_once_2_librewolf_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo extrepo enable librewolf
+sudo apt update
 sudo apt install librewolf
 sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/librewolf 250
 
