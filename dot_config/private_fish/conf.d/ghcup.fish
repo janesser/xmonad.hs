49a6db085363c08a19888e8387276b4f3d725da8
Jan Esser <jesser@gmx.de>
2025-10-07 14:00:08 +0200

    ghcup fixed

--- dot_config/private_fish/conf.d/ghcup.fish
+++ dot_config/private_fish/conf.d/ghcup.fish
@@ -1,3 +1,3 @@
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; 
 
-fish_add_path -g $HOME/.cabal/bin /home/jan/.ghcup/bin $PATH
+fish_add_path -g $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
--- run_once_2_ghcup_install.sh
+++ run_once_2_ghcup_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 GHCUP_ON_PATH=`which ghcup`
-if [ -n $GHCUP_ON_PATH ]; then
+if [ -n "$GHCUP_ON_PATH" ]; then
     echo ghcup already installed.
     exit 0;
 fi
 
-sudo apt install build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev
+sudo apt install build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev libncurses6 libtinfo6 pkg-config
+# answer N to fish.config PATH prepending
 # answer Y to haskell-language-server
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
