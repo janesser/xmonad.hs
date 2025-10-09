f9dca87e5f9183d76681e082805887bf8ed1467a
Jan Esser <jesser@gmx.de>
2025-10-09 13:24:40 +0200

    refine lightdm fix

--- run_once_4_lightdm-session_fix96.sh
+++ run_once_4_lightdm-session_fix96.sh
@@ -2,8 +2,17 @@
 # by this fix lightdm sources $HOME/.profile and adopts adjusted PATH variables etc.
 # https://github.com/canonical/lightdm/issues/96
 
-
-curl -sL https://raw.githubusercontent.com/canonical/lightdm/refs/heads/main/debian/lightdm-session | sudo tee /usr/local/sbin/lightdm-session
-sudo chmod +x /usr/local/sbin/lightdm-session
-sudo sed -i 's/#session-wrapper=lightdm-session/session-wrapper=lightdm-session/' /etc/lightdm/lightdm.conf
-sudo systemctl restart lightdm
+if [ "$CHEZMOI_ARCH" = "arm64" ]
+then
+    curl -sL https://raw.githubusercontent.com/canonical/lightdm/refs/heads/main/debian/lightdm-session | sudo tee /usr/local/sbin/lightdm-session
+    sudo chmod +x /usr/local/sbin/lightdm-session
+    sudo sed -i 's/#session-wrapper=lightdm-session/session-wrapper=lightdm-session/' /etc/lightdm/lightdm.conf
+    if [ -n "$DISPLAY" ];
+    then
+        echo "Reboot or 'sudo systemctl restart lightdm' from outside X"
+    else
+        sudo systemctl restart lightdm
+    fi
+else
+    echo $CHEZMOI_ARCH not supported for lightdm-session fix
+fi
