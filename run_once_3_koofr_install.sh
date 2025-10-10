b30223cfb09196bc527f057c31318d21a05f94f1
Jan Esser <jesser@gmx.de>
2025-10-10 18:12:59 +0200

    koofr install fix

--- run_once_3_koofr_install.sh
+++ run_once_3_koofr_install.sh
@@ -4,10 +4,8 @@ sudo apt install gvfs gvfs-fuse gvfs-backends
 
 INSTALLER_DIR=/tmp/koofr-install/
 
+rm -fR ~/.koofr-dist
 mkdir -p $INSTALLER_DIR
-pushd .
 curl -L --output - https://app.koofr.net/dl/apps/linux64 | tar xzv -C $INSTALLER_DIR
-cd "$INSTALLER_DIR/koofr"
-. ./installer.sh
-popd
+$INSTALLER_DIR/koofr/installer.sh
 rm -fR $INSTALLER_DIR
