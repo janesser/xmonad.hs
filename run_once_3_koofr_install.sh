265372bbe4991a1b3aba0803c2043e191a003b79
Jan Esser <jesser@gmx.de>
2025-10-10 11:44:32 +0200

    rpi progressing slowly

--- run_once_3_koofr_install.sh
+++ run_once_3_koofr_install.sh
@@ -5,6 +5,9 @@ sudo apt install gvfs gvfs-fuse gvfs-backends
 INSTALLER_DIR=/tmp/koofr-install/
 
 mkdir -p $INSTALLER_DIR
+pushd .
 curl -L --output - https://app.koofr.net/dl/apps/linux64 | tar xzv -C $INSTALLER_DIR
-. $INSTALLER_DIR/koofr/installer.sh
+cd "$INSTALLER_DIR/koofr"
+. ./installer.sh
+popd
 rm -fR $INSTALLER_DIR
--- run_once_4_hp_v28_edid_fix.sh
+++ run_once_4_hp_v28_edid_fix.sh
@@ -10,6 +10,7 @@ for HDMI in $(find /sys/class/drm -name "*HDMI*"); do
         sudo cp raspberry/hpv28.bin /usr/lib/firmware/
         if [ -f /boot/firmware/cmdline.txt ] && ! grep drm.edid_firmware /boot/firmware/cmdline.txt
         then
+            # TODO use specific $HMDI substring here
             sudo sed -i 's/$/ drm.edid_firmware=HDMI-A-1:hpv28.bin/' /boot/firmware/cmdline.txt
         fi
     fi
