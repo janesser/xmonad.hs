2b0da00dba65f0e8d9984eeabda8fbd97f15f301
Jan Esser <jesser@gmx.de>
2025-10-30 08:20:00 +0100

    simplified cleanup snap

--- dot_local/bin/executable_cleanup-snap.sh
+++ dot_local/bin/executable_cleanup-snap.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 LANG=C
-
-snap_cleanup() {
-    snap list --all | awk '/disabled/{print $1, $3}' |
-        while read snapname revision; do
-            snap remove "$snapname" --revision="$revision"
-        done
-    echo all disabled revisions cleaned.
-}
-
-export -f snap_cleanup
-
-su "$USER" -s /usr/bin/bash -c 'snap_cleanup'
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
