2db65ddd90a708fa400d58c836cc7494a74bbf99
Jan Esser <jesser@gmx.de>
2025-11-16 13:14:32 +0100

    fail fast for check_host

--- dot_local/bin/executable_check_host_online.sh
+++ dot_local/bin/executable_check_host_online.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-## start with 'check_host_online.sh &;disown'
+## in fish-shell start with 'check_host_online.sh &;disown'
 
 HOST=$1
 LAST_SEEN_ONLINE=never
+CHECK_INTERVAL=${CHECK_INTERVAL:=5}
+
+# fail fast if host can't be reached
+if ping -q -c 2 $HOST >/dev/null
+then
+    LAST_SEEN_ONLINE=`date`
+else
+    exit 1
+fi
+
 
 while true; do
-    if ping -q -c 2 $HOST >/dev/null
+    if ping -q -c 2 "$HOST" >/dev/null
     then
         #echo $HOST responds to ping.
         LAST_SEEN_ONLINE=`date`
     else
         notify-send "$HOST offline" "$HOST offline, last seen online $LAST_SEEN_ONLINE."
     fi
-    sleep 2
+    sleep "$CHECK_INTERVAL"
 done
