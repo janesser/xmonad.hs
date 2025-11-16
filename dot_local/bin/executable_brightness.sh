5fe41a840c163844c87eb6805a0bf298d016150c
Jan Esser <jesser@gmx.de>
2025-11-16 23:13:54 +0100

    fixing yoga510 brightness.sh

--- dot_local/bin/executable_brightness.sh
+++ dot_local/bin/executable_brightness.sh
@@ -2,11 +2,12 @@
 OP=$1
 ACT=`xfpm-power-backlight-helper --get-brightness`
 MAX=`xfpm-power-backlight-helper --get-max-brightness`
+INCR=`expr $MAX / 50`
 
 if [ -z $OP ] ; then
     printf '%3.0f%%' $(($ACT * 100 / $MAX))
 else
-    NEW=$(($ACT $OP 8))
+    NEW=$(($ACT $OP $INCR))
     if [ "$NEW" -gt "$MAX" ] ; then
         NEW=$MAX
     fi
