ffb51b6d776d763ee54a5a6e5437c38ccccfae1e
Jan Esser <jesser@gmx.de>
2025-11-19 12:49:38 +0100

    enable rust/cargo via rustup/smap

--- run_once_3_devtools_0install.sh
+++ run_once_3_devtools_0install.sh
@@ -6,6 +6,9 @@ if [ "$CHEZMOI_ARCH" = "amd64" ]; then
     sudo apt install -y snap
     sudo snap install kubectl --classic
     sudo snap install helm --classic
+    # install rust/cargo
+    sudo snap install rustup --classic
+    rustup default stable
 else
     echo $CHEZMOI_ARCH not supported for snap.
 fi
