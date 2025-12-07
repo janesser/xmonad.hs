e6c368e6e0d6ed90ab3cb33b3b2c522e2044a518
Jan Esser <jesser@gmx.de>
2025-12-07 20:55:06 +0100

    install ollama once

--- run_once_3_devtools_4ollama.sh
+++ run_once_3_devtools_4ollama.sh
@@ -5,4 +5,9 @@
 ## https://github.com/jameschrisa/Ollama_Tuning_Guide/blob/main/docs/cpu-optimization.md
 
 ## https://github.com/ollama/ollama/issues/3976
-# curl -fsSL https://ollama.com/install.sh | sh
+if command -v ollama >/dev/null
+then
+    echo ollama already installed.
+else
+    curl -fsSL https://ollama.com/install.sh | sh
+fi
