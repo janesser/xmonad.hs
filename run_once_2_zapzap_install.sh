b1d17bbf92ce023ae8522ce19759931f12cc4fd8
Jan Esser <jesser@gmx.de>
2025-12-14 21:26:31 +0100

    solidify whatsapp installations

--- run_once_2_zapzap_install.sh
+++ run_once_2_zapzap_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo apt install -y python3-pyqt6 python3-poetry
 mkdir -p ~/projs; cd ~/projs
 
-# TODO complete poetry process
+if cd ~/projs/zapzap
+then
+    echo resetting present env.
+    poetry env remove --all
+fi
