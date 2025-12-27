60f8b8b9f431a2249201ee7115eb85bea6e985de
Jan Esser <jesser@gmx.de>
2025-12-27 11:45:22 +0100

    deprovision broken my-weather-indicator

--- run_once_2_my-weather-indicator_install.sh
+++ run_once_2_my-weather-indicator_install.sh
@@ -2,8 +2,9 @@
 
 if command -v add-apt-repository >/dev/null 2>&1
 then
-    sudo add-apt-repository -y ppa:atareao/atareao
-    sudo apt install -y my-weather-indicator
+    # removal after disfunction FIXME find replacement
+    sudo add-apt-repository -y -r ppa:atareao/atareao
+    sudo apt remove --purge -y my-weather-indicator
 else
     echo add-apt-repository not found to enable my-weather-indicator PPA
 fi
