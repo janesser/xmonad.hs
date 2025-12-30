2e06226513afde4c4abbbb8f638c4e00da4d292a
Jan Esser <jesser@gmx.de>
2025-12-30 10:14:48 +0100

    ssh-agent from (fenv) .profile

--- run_once_1_fish_2fisher.sh
+++ run_once_1_fish_2fisher.sh
@@ -2,6 +2,8 @@
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
+fisher install oh-my-fish/plugin-foreign-env
+
 sudo apt install -y fzf fdclone
 fisher install PatrickF1/fzf.fish
 
