344047672aae0c8c5dfb1b7f35d7d720088fc06c
Jan Esser <jesser@gmx.de>
2026-01-04 21:59:50 +0100

    fix zapzap

--- run_once_2_zapzap_install.sh
+++ run_once_2_zapzap_install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-sudo apt install -y python3-pyqt6 python3-poetry
+ZAPZAP_VERSION=6.2.3.1
+
+sudo apt install -y python3-webview python3-pyqt6 python3-poetry
+
 mkdir -p ~/projs; cd ~/projs
+git clone --depth 1 https://github.com/rafatosta/zapzap.git --single-branch  --branch=$ZAPZAP_VERSION
 
 if cd ~/projs/zapzap
 then
+    git reset --hard $ZAPZAP_VERSION
     echo resetting present env.
     poetry env remove --all
+    poetry self sync
+    poetry install
+    poetry update --sync dbus-python
+else
+    exit 1
 fi
