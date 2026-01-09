9a666d6fd8770b12eee952adda3a59426b0b5a5f
Jan Esser <jesser@gmx.de>
2026-01-09 17:38:21 +0100

    fix github action (local)

--- run_once_3_devtools_github_act_install.sh
+++ run_once_3_devtools_github_act_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo apt install -y podman-docker
+sudo apt install -y podman-docker golang-go
 go install github.com/nektos/act@latest
 
 # fix permission trouble
 ## /var/run/docker.socket points where no one may reach
 sudo chown root:docker /run/podman
 sudo chmod 750 /run/podman # 700 before
-sudo chown root:docker /run/podman/podman.socket
+sudo chown root:docker /run/podman/podman.sock
 sudo chmod 660 /run/podman/podman.socket # 600 before
 
 act --version
