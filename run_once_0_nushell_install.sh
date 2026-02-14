7c598d4607173d444c3e693b1061e28b3d14b613
Jan Esser <jesser@gmx.de>
2026-02-14 22:05:12 +0100

    nu continuation

--- run_once_0_nushell_install.sh
+++ run_once_0_nushell_install.sh
@@ -4,3 +4,6 @@ curl -fsSL https://apt.fury.io/nushell/gpg.key | sudo gpg --dearmor -o /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/fury-nushell.gpg] https://apt.fury.io/nushell/ /" | sudo tee /etc/apt/sources.list.d/fury-nushell.list
 sudo apt update
 sudo apt install -y nushell
+
+# https://github.com/nushell/awesome-nu
+# https://github.com/bevry/dorothy/ <- try this soon
