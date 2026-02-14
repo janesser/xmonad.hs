c5eab150ca45daec1cad3cf7cc0a8a2797291d45
Jan Esser <jesser@gmx.de>
2026-02-14 19:20:09 +0100

    add nushell

---
+++ run_once_0_nushell_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -fsSL https://apt.fury.io/nushell/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/fury-nushell.gpg
+echo "deb [signed-by=/etc/apt/keyrings/fury-nushell.gpg] https://apt.fury.io/nushell/ /" | sudo tee /etc/apt/sources.list.d/fury-nushell.list
+sudo apt update
+sudo apt install -y nushell
