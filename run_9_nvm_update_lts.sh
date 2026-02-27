72a65ab32363c7e0ca0b675bb9370cfcc725c982
Jan Esser <jesser@gmx.de>
2026-02-27 18:05:38 +0100

    improved nvm cleanup

--- run_9_nvm_update_lts.sh
+++ run_9_nvm_update_lts.sh
@@ -6,11 +6,15 @@ then
     nvm install-latest-npm
     
     # https://helloacm.com/how-to-clean-up-nvm-node-versions-except-one/
-    KEEP_VERSION=`nvm ls-remote --lts --no-colors | tail -n 1 | awk '{print $2;}'`
-    for version in $(nvm ls --no-colors --no-alias | \
+    KEEP_VERSION=`nvm ls-remote --lts --no-colors | tail -n 1 | grep -oE "v[0-9\.]+"`
+    echo Keeping version $KEEP_VERSION.
+    # FIXME nvm ls in bash shows less versions than fisher plugin
+    for v in $(nvm ls --no-colors --no-alias | \
             grep -v $KEEP_VERSION | \
-            grep -v system ); do
-        nvm uninstall $version
+            grep -v system | \
+            grep -oE "v[0-9\.]+"); do
+        echo Uninstalling $v.
+        nvm uninstall $v
     done
 else
     echo ~/.nvm/nvm.sh not available \(yet\).
