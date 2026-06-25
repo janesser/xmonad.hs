9cc3cacf9ffa396467163c715fd19ff6bbb4fa9c
Jan Esser <jesser@gmx.de>
2026-06-25 07:07:20 +0200

    housekeeping

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -11,3 +11,10 @@ set -gx PATH $PATH ~/.lmstudio/bin
 # End of LM Studio CLI section
 
 set -gx PATH ~/.pyenv/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/jan/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
