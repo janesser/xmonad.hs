37a98c81e4874e6c885f5d10eea2156f91f2d6d3
Jan Esser <jesser@gmx.de>
2026-06-28 22:24:26 +0200

    pi-agent improvements

--- .chezmoiscripts/run_once_5_aitools_3agentic.sh
+++ .chezmoiscripts/run_once_5_aitools_3agentic.sh
@@ -4,12 +4,16 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 nvm use --lts
+sudo apt install -y fd-find
+## https://github.com/earendil-works/pi/issues/3882
+ln -sf /usr/bin/fdfind ~/.pi/agent/bin/fd
 npm i -g @earendil-works/pi-coding-agent
-npx skills ls
 
 # use pi with local llama.cpp
 ## https://github.com/opensecurity/code-offline/tree/main
 ## see 'pi-update-llama-models.sh'
+
+pi install npm:pi-web-access
 
 uvx hf download hf://nvidia/NVIDIA-Nemotron-3-Nano-4B-GGUF/NVIDIA-Nemotron3-Nano-4B-Q4_K_M.gguf
 uvx hf download hf://google/gemma-4-E4B-it-qat-q4_0-gguf/gemma-4-E4B-it-mmproj.gguf
--- dot_local/bin/executable_pi-update-llama-models.sh
+++ dot_local/bin/executable_pi-update-llama-models.sh
@@ -9,11 +9,15 @@ set -euo pipefail
 JSON_FILE="/home/jan/.pi/agent/models.json"
 
 MODEL_IDS_JSON=$(llama cli -cl|sed -n "s/^\s*[0-9]\.\s\(.*\)/\1/p"|jq -R '{ id : . }' | jq -s)
-echo $MODEL_IDS_JSON
+#echo "$MODEL_IDS_JSON"
 
-cp "$JSON_FILE" "$JSON_FILE.old"
-jq --argjson models "$MODEL_IDS_JSON" '.providers."llama-cpp".models = $models' "$JSON_FILE.old" > "$JSON_FILE"
+TEMPLATE_FILE="$JSON_FILE.template"
+if [[ ! -f "$TEMPLATE_FILE" ]];then
+  cp "$JSON_FILE" "$TEMPLATE_FILE"
+  chezmoi add "$TEMPLATE_FILE"
+  echo "$JSON_FILE.template created."
+fi
+
+jq --argjson models "$MODEL_IDS_JSON" '.providers."llama-cpp".models = $models' "$TEMPLATE_FILE" > "$JSON_FILE"
 
 echo "✅ models.json updated with IDs: $MODEL_IDS_JSON"
-
-chezmoi re-add "$JSON_FILE"
