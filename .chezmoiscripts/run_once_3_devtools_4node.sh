c9e92ed26f253a83ea64b51d16a8507ec5adb9de
Jan Esser <jesser@gmx.de>
2026-08-02 22:32:44 +0200

    bugfixing nvm install script

--- .chezmoiscripts/run_once_3_devtools_4node.sh
+++ .chezmoiscripts/run_once_3_devtools_4node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-bass source $NVM_DIR/nvm.sh ';' nvm install --lts --latest-npm
+bass source ~/.nvm/nvm.sh ';' nvm install --lts --latest-npm
 
 fisher install jorgebucaran/nvm.fish
 
@@ -11,9 +11,10 @@ fisher install jorgebucaran/nvm.fish
 ## NVM_DIR is set in .profile, nvm-fish defaults apply from ~/.config/nvm.fish
 
 # FIX align $nvm_data with $NVM_DIR
-echo "set --global nvm_data $NVM_DIR/versions/node" > ~/.config/fish/conf.d/0nvm.fish
+echo "set --global nvm_data \$NVM_DIR/versions/node" > ~/.config/fish/conf.d/0nvm.fish
 
-set --global nvm_data $NVM_DIR/versions/node # oneshot
-cp ~/.local/share/nvm/.index ~/.nvm/versions/node/
+set --global nvm_data ~/.nvm/versions/node # oneshot
+#cp ~/.local/share/nvm/.index ~/.nvm/versions/node/
 
+nvm ls-remote
 nvm use lts
