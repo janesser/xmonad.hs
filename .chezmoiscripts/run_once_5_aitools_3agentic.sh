5c4d84cd4351e69f49093ed0f758849d9ab416cd
Jan Esser <jesser@gmx.de>
2026-08-02 23:10:10 +0200

    insane animations in pi-agent

--- .chezmoiscripts/run_once_5_aitools_3agentic.sh
+++ .chezmoiscripts/run_once_5_aitools_3agentic.sh
@@ -13,6 +13,7 @@ ln -sf /usr/bin/fdfind ~/.pi/agent/bin/fd
 npx @robzolkos/lazypi --yes
 
 pi install npm:@hypabolic/crossbar
+pi install npm:pi-animations
 
 pi uninstall npm:pi-claude-cli
 pi list |grep -o 'npm:@juicesharp/rpiv-.*'| xargs -L1 pi uninstall
