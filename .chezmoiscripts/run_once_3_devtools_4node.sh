b42751cf771fb4ed50c5f596701a44e6e4a2aaeb
Jan Esser <jesser@gmx.de>
2026-08-02 08:13:00 +0200

    permanent fix for nvm-fish to nvm-sh misalignment

--- .chezmoiscripts/run_once_3_devtools_4node.sh
+++ .chezmoiscripts/run_once_3_devtools_4node.sh
@@ -4,7 +4,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 bass source $NVM_DIR/nvm.sh ';' nvm install --lts --latest-npm
 
 fisher install jorgebucaran/nvm.fish
-set --global nvm_data $NVM_DIR
+
+# SYMPTOM nvm ls in bash shows less versions than fisher plugin
+## check $NVM_DIR what native nvm uses ~/.nvm
+## check $nvm_data what nvm.fish uses ~/.local/share/nvm
+## NVM_DIR is set in .profile, nvm-fish defaults apply from ~/.config/nvm.fish
+
+# FIX align $nvm_data with $NVM_DIR
+echo "set --global nvm_data $NVM_DIR/versions/node" > ~/.config/fish/conf.d/0nvm.fish
+
+set --global nvm_data $NVM_DIR/versions/node # oneshot
+cp ~/.local/share/nvm/.index ~/.nvm/versions/node/
 
 nvm use lts
-npm i -g pnpm
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,16 +5,3 @@ end
 status is-interactive; and begin
     set fish_tmux_autostart false
 end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH ~/.lmstudio/bin
-# End of LM Studio CLI section
-
-set -gx PATH ~/.pyenv/bin $PATH
-
-# pnpm
-set -gx PNPM_HOME "/home/jan/.local/share/pnpm"
-if not string match -q -- "$PNPM_HOME/bin" $PATH
-  set -gx PATH "$PNPM_HOME/bin" $PATH
-end
-# pnpm end
