bfec102817c01124695ed70e867bbfc56a2d93ca
Jan Esser <jesser@gmx.de>
2026-09-03 19:51:57 +0200

    optimized ornith preset

--- dot_local/bin/executable_revise-llama-models-preset.sh
+++ dot_local/bin/executable_revise-llama-models-preset.sh
@@ -2,10 +2,12 @@
 
 LLAMA_MODELS_PRESET=~/.llama-cpp-models-preset.ini
 
-chezmoi edit -a  ~/.llama-cpp-models-preset.ini
+hx ~/.llama-cpp-models-preset.ini
 
 if [[ -n `chezmoi diff $LLAMA_MODELS_PRESET` ]]; then
   restart-llama-server.sh
 fi
+
+chezmoi re-add ~/.llama-cpp-models-preset.ini
 
 tail -f ~/.local/log/llama-server.log
