1a2f84726a16c55dc0f848e57ba742a33dc5c948
Jan Esser <jesser@gmx.de>
2026-09-03 21:14:16 +0200

    clean unmanaged chezmoi configs but asdf.fish

--- .chezmoiscripts/run_9_update_fish.sh
+++ .chezmoiscripts/run_9_update_fish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/fish
 
-chezmoi add ~/.config/fish/conf.d/asdf.fish
-
 # cleanup unmanaged files
 for f in $(cz unmanaged ~/.config/fish/conf.d)
-    rm $f
+    if [ "$(basename $f)" != "asdf.fish" ]
+        rm $f
+    end
 end
 
 # update plugins
