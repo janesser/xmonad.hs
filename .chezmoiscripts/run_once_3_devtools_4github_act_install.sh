0803a554779a3c3209edb5457576a9afc589eb03
Jan Esser <jesser@gmx.de>
2026-09-03 19:59:49 +0200

    fix podman installation

--- .chezmoiscripts/run_once_3_devtools_4github_act_install.sh
+++ .chezmoiscripts/run_once_3_devtools_4github_act_install.sh
@@ -5,9 +5,15 @@ go install github.com/nektos/act@latest
 
 # fix permission trouble
 ## /var/run/docker.socket points where no one may reach
+
+sudo groupadd docker # might not exist
+
 sudo chown root:docker /run/podman
 sudo chmod 750 /run/podman # 700 before
-sudo chown root:docker /run/podman/podman.sock
-sudo chmod 660 /run/podman/podman.sock # 600 before
+sudo bash -c "chown root:docker /run/podman/*.sock"
+sudo bash -c "chmod 660 /run/podman/*.sock" # 600 before
 
+sudo usermod -a -G docker $USER
+
+export PATH=~/go/bin:$PATH
 act --version
