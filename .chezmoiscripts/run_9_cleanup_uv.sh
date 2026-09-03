c6702932b5c48d1890d738db1a709e0bc6986983
Jan Esser <jesser@gmx.de>
2026-09-03 21:01:16 +0200

    safeguard uv cache clean

--- .chezmoiscripts/run_9_cleanup_uv.sh
+++ .chezmoiscripts/run_9_cleanup_uv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=~/go/bin:~/.asdf/shims:$PATH # in case not yet set
+
 # TODO clean-up old python versions
 ## uv python list --only-installed --managed-python
 uv cache clean
